1338fbe6125cc0c21c8ec56f38e2de6160053928
eitanakash <eitan.n@trgint.com>
2020-04-26 16:08:05 +0300

    add redis

--- nestjs.micro.client-finish/src/math.service.ts
+++ nestjs.micro.client-finish/src/math.service.ts
@@ -7,10 +7,9 @@ export class MathService {
 
   constructor() {
     this.client = ClientProxyFactory.create({
-      transport: Transport.TCP,
+      transport: Transport.REDIS,
       options: {
-        host: '127.0.0.1',
-        port: 8877,
+        url: 'redis://localhost:6379',
       },
     });
   }
